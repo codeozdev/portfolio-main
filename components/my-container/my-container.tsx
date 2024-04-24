449d741484a6635b970ad6c6c4f95753d9b3cd66
ozudev <ozudev@gmail.com>
2024-04-24 20:38:19 +0300

    dark mode mobile created

--- components/my-container/my-container.tsx
+++ components/my-container/my-container.tsx
@@ -4,22 +4,25 @@ import Link from "next/link";
 
 export default function MyContainer() {
   const li = `hover:scale-110 duration-500 ease-in-out cursor-pointer border border-red-500 dark:border-[#8750F7] rounded-full fill-[#8750F7] p-2`;
+  const mid = `md:max-w-[400px] md:max-h-[440px]`;
+  const isDark = `dark:bg-[#140C1C] dark:border-[#8650F6] dark:border`;
 
   return (
     <div className="w-full h-full relative">
-      <div className="p-10 relative z-20">
-        <div className="grid grid-cols-2 place-items-center">
+      <div className="p-5 md:p-10 relative z-20">
+        {/* GRID */}
+        <div className="grid grid-cols-1 gap-10 md:gap-0 md:grid-cols-2 place-items-center">
           <div className="w-full h-fit">
             <h3>I am Oguz</h3>
-            <h1 className="max-w-[600px] tracking-wider leading-tight">
+            <h1 className="max-w-[300px] md:max-w-[600px] tracking-wider leading-tight">
               Web Developer + UX Designer
             </h1>
-            <p className="font-sora text-xl tracking-wide min-w-[500px]">
+            <p className="font-sora text-center md:text-start md:text-xl tracking-wide md:min-w-[500px]">
               I break down complex user experinece problems to create integritiy focussed solutions
               that connect billions of people
             </p>
 
-            <ul className="flex gap-5 mt-10 ">
+            <ul className="flex gap-5 mt-10 items-center md:justify-start justify-center">
               <li className={li}>
                 <Link href="#">
                   <BsTwitterX />
@@ -43,7 +46,7 @@ export default function MyContainer() {
               alt="img"
               width={500}
               height={500}
-              className={`max-w-[400px] max-h-[440px] object-cover dark:bg-[#140C1C] dark:border-[#8650F6] dark:border rounded-3xl grayscale hover:rotate-0 rotate-6 transition-all duration-300 ease-in-out hover:grayscale-0`}
+              className={`max-w-52 max-h-max object-cover grayscale hover:rotate-0 rotate-6 transition-all duration-300 ease-in-out hover:grayscale-0 rounded-3xl ${mid} ${isDark}`}
               sizes="60vw"
             />
           </div>
